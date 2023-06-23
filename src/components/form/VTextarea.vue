b50beaa816ec365eef906af95930e2e9b4da20a2
Remy Zulauff <remy.zulauff@gmail.com>
2023-06-23 17:42:10 +0200

    style(form): reworked input files style

--- src/components/form/VTextarea.vue
+++ src/components/form/VTextarea.vue
@@ -31,7 +31,7 @@ const { value, errorMessage } = useField(identifier, required.value ? isRequired
       :id="identifier"
       v-model="value"
       :rows="rows"
-      class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+      class="block w-full rounded-lg border-0 px-3.5 py-2 text-gray-900 bg-gray-50 ring-1 ring-inset ring-black ring-opacity-5 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
     />
   </div>
   <p class="mt-2 text-sm text-red-600" v-if="errorMessage">{{ errorMessage }}</p>
