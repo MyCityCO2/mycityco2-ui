b30295f3edc4655c0d46eca0478bdff371e48bec
Remy Zulauff <remy.zulauff@gmail.com>
2023-06-26 14:35:40 +0200

    revert(footer): switch back to old footer and remove useless social links

--- src/layouts/TheFooter.vue
+++ src/layouts/TheFooter.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { navigation as footerLinks } from '@/constant'
 import { socials } from '@/svg'
+import { RouterLink } from 'vue-router'
 
 const navigation = {
   main: footerLinks,
@@ -9,58 +10,32 @@ const navigation = {
 </script>
 
 <template>
-  <footer class="bg-gray-50 sm:sticky sm:top-auto sm:left-0 sm:bottom-0 sm:right-0">
-    <div class="flex flex-col items-center overflow-hidden py-10 sm:py-16">
-      <div class="container relative m-auto px-6 md:px-12">
-        <div class="m-auto md:w-10/12 lg:w-8/12 xl:w-6/12">
-          <div class="flex flex-wrap items-center justify-between md:flex-nowrap">
-            <div
-              class="flex w-full justify-center space-x-12 text-gray-600 sm:w-7/12 md:justify-start"
-            >
-              <ul class="list-inside list-disc space-y-8">
-                <li v-for="item in navigation.main" :key="item.name">
-                  <RouterLink class="transition hover:text-primary" :to="item.href">{{
-                    item.name
-                  }}</RouterLink>
-                </li>
-              </ul>
-
-              <!-- <ul role="list" class="space-y-8">
-                <li v-for="item in navigation.social" :key="item.name">
-                  <a
-                    :href="item.href"
-                    target="_blank"
-                    class="flex items-center space-x-3 transition hover:text-primary"
-                  >
-                    <component :is="item.icon" class="w-5" />
-                    <span>{{ item.name }}</span>
-                  </a>
-                </li>
-              </ul> -->
-            </div>
-            <div
-              class="m-auto mt-16 w-10/12 space-y-6 text-center sm:mt-auto sm:w-5/12 sm:text-left"
-            >
-              <span class="block text-gray-500"
-                >On ne peut pas améliorer ce qu’on ne sait pas compter</span
-              >
-              <span class="block text-gray-500"
-                >Besoin d'aide ?
-                <RouterLink to="/contact" class="font-semibold">Contactez-nous</RouterLink></span
-              >
-
-              <span class="block text-gray-500"
-                >&copy; <span id="year">2023 MyCityCO2. All rights reserved.</span></span
-              >
-
-              <!-- <span class="flex justify-between">
-                <a href="#" class="font-semibold">Terms of Use</a>
-                <a href="#" class="font-semibold">Privacy Policy</a>
-              </span> -->
-            </div>
-          </div>
+  <footer class="bg-gray-50 sticky top-auto left-0 bottom-0 right-0">
+    <div class="mx-auto max-w-7xl overflow-hidden px-6 py-10 sm:py-16 lg:px-8">
+      <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+        <div v-for="item in navigation.main" :key="item.name" class="pb-6">
+          <RouterLink
+            :to="item.href"
+            class="text-sm leading-6 text-gray-600 hover:text-green-600 transition-all"
+            >{{ item.name }}</RouterLink
+          >
         </div>
+      </nav>
+      <div class="mt-10 flex justify-center space-x-10">
+        <a
+          v-for="item in navigation.social"
+          :key="item.name"
+          :href="item.href"
+          target="_blank"
+          class="text-gray-400 hover:text-green-500 transition-all"
+        >
+          <span class="sr-only">{{ item.name }}</span>
+          <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+        </a>
       </div>
+      <p class="mt-10 text-center text-xs leading-5 text-gray-500">
+        &copy; 2023 MyCityCO2, Inc. All rights reserved.
+      </p>
     </div>
   </footer>
 </template>
