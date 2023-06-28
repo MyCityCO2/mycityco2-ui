5fb9320749ccc0bbde3d5eb2c19914f6fc56f48b
Remy Zulauff <remy.zulauff@gmail.com>
2023-06-28 16:32:58 +0200

    chore(eslint): update .eslintrc.cjs to include node environment for .cjs files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  overrides: [
+    {
+      files: ['**/*.cjs'],
+      env: {
+        node: true
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest'
