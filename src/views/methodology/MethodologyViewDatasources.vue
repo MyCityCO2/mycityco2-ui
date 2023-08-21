ff448003857c8cf92858b6e90f5e1a55ec927c77
Remy Zulauff <rzu@open-net.ch>
2023-08-21 15:49:49 +0200

    fix(MethodologyViewDatasources.vue): replace 'limits' property with 'link' property in the datasources array to provide a clickable link to the data source style(MethodologyViewDatasources.vue): add max width to the container to limit its size and improve layout aesthetics

--- src/views/methodology/MethodologyViewDatasources.vue
+++ src/views/methodology/MethodologyViewDatasources.vue
@@ -6,7 +6,7 @@ const datasources = [
     year: 'Depuis 2010 jusqu’à 2021 tous les ans',
     geo: 'Par commune pour toute la France',
     vendor: 'DGFiP (Direction Générale des Finances Publiques)',
-    limits: ''
+    link: 'https://data.economie.gouv.fr/explore/?q=balances+comptables+des+communes&sort=title'
   },
   {
     name: 'Facteurs d’émissions monétaires',
@@ -14,7 +14,7 @@ const datasources = [
     year: 'Depuis 1995 jusqu’à 2022 tous les ans',
     geo: '44 pays dont les pays de l’UE',
     vendor: 'Exiobase',
-    limits: ''
+    link: 'https://zenodo.org/record/5589597'
   }
 ]
 </script>
@@ -26,7 +26,7 @@ const datasources = [
     </div>
     <div class="flex flex-col methodology__section">
       <div
-        class="overflow-hidden shadow rounded sm:rounded-lg"
+        class="overflow-hidden shadow rounded sm:rounded-lg max-w-3xl"
         v-for="data in datasources"
         :key="data.name"
       >
@@ -63,9 +63,9 @@ const datasources = [
               </dd>
             </div>
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-gray-900">Limites</dt>
+              <dt class="text-sm font-medium text-gray-900">Lien</dt>
               <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                {{ data.limits }}
+                <a :href="data.link" target="_blank" class="text-link">{{ data.link }}</a>
               </dd>
             </div>
           </dl>
