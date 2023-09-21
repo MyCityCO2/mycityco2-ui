c264701794c739e6187ef9e897fc5ce05a9e5718
Remy Zulauff <remy.zulauff@gmail.com>
2023-09-21 15:58:42 +0200

    refactor: Remove pwa for now

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import robotsPlugin from './robotsPlugin'
 import sitemapPlugin from './sitemapPlugin'
 
@@ -12,38 +11,7 @@ process.env = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    robotsPlugin(),
-    sitemapPlugin(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      workbox: {
-        sourcemap: true
-      },
-      manifest: {
-        name: 'MyCityCo2',
-        short_name: 'MyCityCo2',
-        description: 'MyCityCo2 web application',
-        theme_color: '#47b9a3',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            type: 'image/png',
-            sizes: '512x512'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [vue(), robotsPlugin(), sitemapPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
