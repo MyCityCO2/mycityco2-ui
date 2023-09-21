86d7ee4b137075fd3ab5e1cb0a2d8f9e1df70a7a
Remy Zulauff <remy.zulauff@gmail.com>
2023-09-21 11:19:25 +0200

    feat: Add google tag manager integration

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './assets/main.css'
-
+import { createGtm } from '@gtm-support/vue-gtm'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { createPinia } from 'pinia'
 import { createApp, h, provide } from 'vue'
 import { apolloClient } from './apollo'
+import './assets/main.css'
 import { number } from './directives'
 
 import App from './App.vue'
@@ -17,5 +17,15 @@ createApp({
 })
   .use(createPinia())
   .use(router)
+  .use(
+    createGtm({
+      id: import.meta.env.VITE_GTM,
+      defer: true,
+      compatibility: false,
+      debug: true,
+      enabled: import.meta.env.PROD ? true : false,
+      vueRouter: router
+    })
+  )
   .directive('number', number)
   .mount('#app')
