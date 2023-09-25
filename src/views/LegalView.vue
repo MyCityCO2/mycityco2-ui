2fc654fb299d35bfd340bb6131076314ab10a464
Remy Zulauff <remy.zulauff@gmail.com>
2023-09-25 16:01:10 +0200

    feat(LegalView): implement legal view

--- src/views/LegalView.vue
+++ src/views/LegalView.vue
@@ -5,8 +5,51 @@ import VHeading from '@/components/VHeading.vue'
 <template>
   <div>
     <VHeading title="Mentions légales" text="" alt />
-    <section>
-      <p>This is legal mentions</p>
-    </section>
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 pb-24 space-y-5">
+      <p>
+        Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en
+        l'économie numérique, il est précisé aux utilisateurs du site MyCityCO2 l'identité des
+        différents intervenants dans le cadre de sa réalisation et de son suivi.
+      </p>
+      <h4>Edition du site</h4>
+      <p>
+        Le présent site, accessible à l’URL www.mycityco2.org (le « Site »), est édité par : Open
+        Net S. à r.l. , société au capital de 100000 euros, inscrite au R.C.S. de PARIS sous le
+        numéro CH-550.1.020.926-6, dont le siège social est situé au Chemin de la Lande 1 1008
+        Prilly, représenté(e) par Jacques-André Eberhard dûment habilité(e)
+      </p>
+      <p>Le numéro individuel TVA de l’éditeur est : CHE-101.430.355 TVA.</p>
+      <h4>Hébergement</h4>
+      <p>
+        Le Site est hébergé par la société Infomaniak, situé Rue Eugène-Marziano 25, 1227 Genève,
+        Suisse, (contact téléphonique ou email : (+41) 22 820 35 44).
+      </p>
+      <h4>Directeur de publication</h4>
+      <p>Le Directeur de la publication du Site est Jacques-André Eberhard.</p>
+      <h4>Nous contacter</h4>
+      <ul class="list-disc pl-6 text-gray-600">
+        <li>Par téléphone: +41217014245</li>
+        <li>Par email: contact@mycityco2.org</li>
+        <li>Par courrier: Rue de Genève 77 1004 Lausanne, Suisse</li>
+      </ul>
+      <h4>Données personnelles</h4>
+      <p>
+        Le traitement de vos données à caractère personnel est régi par notre Charte du respect de
+        la vie privée, disponible depuis la section "Charte de Protection des Données Personnelles",
+        conformément au Règlement Général sur la Protection des Données 2016/679 du 27 avril 2016
+        («RGPD»).
+      </p>
+      <p>
+        Jacques-André Eberhard a désigné un Délégué à la Protection des Données (DPO) auprès de la
+        CNIL (Désignation N° ). Les coordonnées de notre Délégué à la Protection des Données sont
+        les suivantes :
+      </p>
+      <p>
+        Nom : Jean-Marc Vandel<br />
+        Adresse : Rue de Genève 77 1004 Lausanne<br />
+        Téléphone : +41217014245<br />
+        Email : contact@mycityco2.org<br />
+      </p>
+    </div>
   </div>
 </template>
