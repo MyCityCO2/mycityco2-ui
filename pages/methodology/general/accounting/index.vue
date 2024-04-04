808a82780852daceef00c0af19b1f13c07fa0d86
jacopobacci <bacci.jaco@gmail.com>
2024-04-04 11:52:42 +0200

    feat: intro translation

--- pages/methodology/general/accounting/index.vue
+++ pages/methodology/general/accounting/index.vue
@@ -1,16 +1,15 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
 <template>
   <div>
     <div class="methodology__section_header">
-      <h3>Introduction</h3>
+      <h3>{{ t("methodology.accounting.intro") }}</h3>
     </div>
     <div class="methodology__section">
       <p>
-        La comptabilité publique suit les principes posés par le plan comptable
-        général applicable au secteur privé : comptabilité en droits constatés,
-        en partie double (correspondance entre les ressources et leurs emplois).
-        Pour les dépenses comme pour les recettes, les collectivités distinguent
-        les opérations de fonctionnement (produits et charges) de ses opérations
-        d'investissement (opérations sur le patrimoine).
+        {{ t("methodology.accounting.description") }}
       </p>
     </div>
   </div>
