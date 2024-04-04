789d22138ad530197c938b54ccf4e077c59469c9
jacopobacci <bacci.jaco@gmail.com>
2024-04-04 16:35:15 +0200

    feat: translate data sources

--- pages/methodology/datasources.vue
+++ pages/methodology/datasources.vue
@@ -1,86 +1,77 @@
 <script setup lang="ts">
-const datasources = [
-  {
-    name: "Balances comptables des communes",
-    definition:
-      "Balances des budgets principaux et budgets annexes des communes",
-    year: "Depuis 2010 jusqu’à 2021 tous les ans",
-    geo: "Par commune pour toute la France",
-    vendor: "DGFiP (Direction Générale des Finances Publiques)",
-    link: "https://data.economie.gouv.fr/explore/?q=balances+comptables+des+communes&sort=title",
-  },
-  {
-    name: "Facteurs d’émissions monétaires",
-    definition:
-      "140 facteurs d’émissions par pays couvrant de nombreux secteurs d’activités",
-    year: "Depuis 1995 jusqu’à 2020 tous les ans",
-    geo: "44 pays dont les pays de l’UE",
-    vendor: "Exiobase",
-    link: "https://zenodo.org/record/5589597",
-  },
-]
+const { t, rt, tm } = useI18n()
 </script>
 
 <template>
   <div>
     <div class="methodology__section_header">
-      <h3>Sources de données</h3>
+      <h3>{{ t("methodology.data_sources.title") }}</h3>
     </div>
     <div class="flex flex-col methodology__section">
       <div
         class="overflow-hidden shadow rounded sm:rounded-lg max-w-3xl"
-        v-for="data in datasources"
-        :key="data.name"
+        v-for="data in tm('methodology.data_sources.list')"
+        :key="rt(data.name)"
       >
         <div class="px-4 py-6 sm:px-6">
           <h3 class="text-base font-semibold leading-7 text-neutral-900">
-            {{ data.name }}
+            {{ rt(data.name) }}
           </h3>
           <p class="mt-1 max-w-2xl text-sm leading-6 text-neutral-500">
-            Détails complet de la source de données
+            {{ t("methodology.data_sources.details") }}
           </p>
         </div>
         <div class="border-t border-neutral-100">
           <dl class="divide-y divide-neutral-100">
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-neutral-900">Définition</dt>
+              <dt class="text-sm font-medium text-neutral-900">
+                {{ t("definition") }}
+              </dt>
               <dd
                 class="mt-1 text-sm leading-6 text-neutral-700 sm:col-span-2 sm:mt-0"
               >
-                {{ data.definition }}
+                {{ rt(data.definition) }}
               </dd>
             </div>
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-neutral-900">Année(s)</dt>
+              <dt class="text-sm font-medium text-neutral-900">
+                {{ t("year", 2) }}
+              </dt>
               <dd
                 class="mt-1 text-sm leading-6 text-neutral-700 sm:col-span-2 sm:mt-0"
               >
-                {{ data.year }}
+                {{ rt(data.year) }}
               </dd>
             </div>
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-neutral-900">Géographie</dt>
+              <dt class="text-sm font-medium text-neutral-900">
+                {{ t("geography") }}
+              </dt>
               <dd
                 class="mt-1 text-sm leading-6 text-neutral-700 sm:col-span-2 sm:mt-0"
               >
-                {{ data.geo }}
+                {{ rt(data.geo) }}
               </dd>
             </div>
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-neutral-900">Fournisseur</dt>
+              <dt class="text-sm font-medium text-neutral-900">
+                {{ t("supplier") }}
+              </dt>
               <dd
                 class="mt-1 text-sm leading-6 text-neutral-700 sm:col-span-2 sm:mt-0"
               >
-                {{ data.vendor }}
+                {{ rt(data.vendor) }}
               </dd>
             </div>
             <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt class="text-sm font-medium text-neutral-900">Lien</dt>
+              <dt class="text-sm font-medium text-neutral-900">
+                {{ t("link") }}
+              </dt>
               <dd
                 class="mt-1 text-sm leading-6 text-neutral-700 sm:col-span-2 sm:mt-0"
               >
-                <a :href="data.link" target="_blank" class="text-link">{{
-                  data.link
+                <a :href="rt(data.link)" target="_blank" class="text-link">{{
+                  rt(data.link)
                 }}</a>
               </dd>
             </div>
