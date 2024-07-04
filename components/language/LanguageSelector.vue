49a115b3e0c40a5b19a0a0fee03126e59f1e5e75
jacopobacci <bacci.jaco@gmail.com>
2024-07-04 10:57:25 +0200

    style(LanguageSelector.vue): comment out unused countryStore variable and availableLocales computed property

--- components/language/LanguageSelector.vue
+++ components/language/LanguageSelector.vue
@@ -1,29 +1,29 @@
 <script setup>
 const { locale: selectedLocale, locales } = useI18n()
 const switchLocalePath = useSwitchLocalePath()
-const countryStore = useCountryStore()
+// const countryStore = useCountryStore()
 
-const availableLocales = computed(() => {
-  return locales.value.filter((i) => i.code.includes(countryStore.country))
-})
+// const availableLocales = computed(() => {
+//   return locales.value.filter((i) => i.code.includes(countryStore.country))
+// })
 </script>
 
 <template>
   <div>
     <div
-      v-for="locale in availableLocales"
+      v-for="locale in locales"
       :key="locale.code"
       class="text-gray-900 group"
     >
       <NuxtLink
         :class="{
-          'text-primary': locale.code === selectedLocale,
+          'text-primary dark:text-primary': locale.code === selectedLocale,
         }"
         :to="switchLocalePath(locale.code)"
       >
         {{ locale.name }}
       </NuxtLink>
-      <span class="group-last:hidden ml-1">|</span>
+      <span class="group-last:hidden ml-1 dark:text-white">|</span>
     </div>
   </div>
 </template>
